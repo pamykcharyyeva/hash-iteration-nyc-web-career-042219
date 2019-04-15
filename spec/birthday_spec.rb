98444b3d06b005ff06c531d655f04d9adbe6a246
Pamyk Charyyeva <pamyk.ch@gmail.com>
2019-04-15 00:51:06 +0000

    Automatically backed up by Learn

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -1,3 +1,4 @@
+
 require_relative "../lib/birthday.rb"
 
 describe "#happy_birthday" do
@@ -15,7 +16,7 @@ end
 
 ## BONUS ##
 describe "#age_appropriate_birthday" do
-  pending it "only prints the birthday greeting if the birthday kid 12 or younger" do
+  it "only prints the birthday greeting if the birthday kid 12 or younger" do
     birthday_kids = {
       "Timmy" => 9,
       "Sarah" => 6,
